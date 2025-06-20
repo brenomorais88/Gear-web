d6225502adc20f06dac3215352c5c3420ca18fd8
Breno Morais <brenomorais88@gmail.com>
2025-06-20 14:39:23 -0300

    Details screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,19 @@
 // src/app/layout.tsx
-import "./globals.css";
+import './globals.css'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import { ReactNode } from 'react'
 
-export default function RootLayout({ children }) {
+interface RootLayoutProps {
+  children: ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="pt-BR">
-      <body>
-        {children}
+      <body className="flex flex-col min-h-screen">
+        <main className="flex-1">{children}</main>
       </body>
     </html>
-  );
+  )
 }
