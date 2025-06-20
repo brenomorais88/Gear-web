643a4f64e6d4ec05151eb470139b746445f6f4d7
Breno Morais <brenomorais88@gmail.com>
2025-06-20 14:35:07 -0300

    Details screen

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['storage.googleapis.com'],
   },
-  experimental: { appDir: true },
   eslint: {
     // ignora erros de lint no build de produção
     ignoreDuringBuilds: true,
